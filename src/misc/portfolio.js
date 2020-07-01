61b0537d937ff03769c27070a68d4a383d11d2b2
Shannon <sgf58@mst.edu>
2020-07-01 08:47:09 -0500

    mob next [ci-skip]

--- src/misc/portfolio.js
+++ src/misc/portfolio.js
@@ -1,7 +1,8 @@
 export const createPortfolio = () => new Map()
 export const uniqueSymbolCount = portfolio => portfolio.size
+export const symbolShareCount = (portfolio, symbolName) => portfolio.get(symbolName) || 0
 export const isEmpty = portfolio => uniqueSymbolCount(portfolio) === 0
-export const purchase = (portfolio, symbolName) => {
-  portfolio.set(symbolName, 1)
+export const purchase = (portfolio, symbolName, symbolShares) => {
+  portfolio.set(symbolName, symbolShareCount(portfolio, symbolName) + symbolShares)
   return portfolio
 }
