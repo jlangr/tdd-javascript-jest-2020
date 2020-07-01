9be7ffb431d50a172f0b9120f06185ba0ca47cc9
Todd Kirk <tkirk@monsanto.com>
2020-07-01 11:36:58 -0500

    mob next [ci-skip]

--- src/pos/routes/checkout.test.js
+++ src/pos/routes/checkout.test.js
@@ -165,20 +165,20 @@ describe('checkout routes', () => {
     it('does stuff', () => {
       IncrementingIdGenerator.reset(checkoutId)
       postCheckout({}, response)
-      overrideRetrieveItem(() => {})
+
       // set up for discountng
       overrideRetrieveItem(() => ({ upc: '333', price: 3.33, description: '', exempt: false }))
       postItem({ params: { id: checkoutId }, body: { upc: '333' } }, response)
-      overrideRetrieveItem(() => {})
-      console.log('req id', checkoutId )
+
+
       overrideRetrieveItem(() => ({ upc: '444', price: 4.44, description: '', exempt: false }))
       postItem({ params: { id: checkoutId }, body: { upc: '444' } }, response)
-      overrideRetrieveItem(() => {})
+
       const request = { params: { id: checkoutId }}
       response = createEmptyResponse()
       postCheckoutTotal(request, response)
       expect(response.status).toEqual(200)
-      console.log('reseponse status', response.status)
+
       const firstCallFirstArg = response.send.mock.calls[0][0]
       expect(firstCallFirstArg).toMatchObject({ total: 7.77 })
       //  not found
